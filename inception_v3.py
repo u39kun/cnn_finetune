9a3de73acd0af61a49b85a6284a81cdf72a58b3f
lef-fan <teri_fan_dak@hotmail.com>
2017-05-24 17:31:35 +0300

    Updated to Keras 2.0.3, Tensorflow 1.1.0

--- inception_v3.py
+++ inception_v3.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from keras.models import Sequential
 from keras.optimizers import SGD
-from keras.layers import Input, Dense, Convolution2D, MaxPooling2D, AveragePooling2D, ZeroPadding2D, Dropout, Flatten, merge, Reshape, Activation
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, AveragePooling2D, Flatten, Activation
+from keras.layers.merge import concatenate
 from keras.layers.normalization import BatchNormalization
 from keras.models import Model
 from keras import backend as K
-
-from sklearn.metrics import log_loss
-
-from load_cifar10 import load_cifar10_data
 
 def conv2d_bn(x, nb_filter, nb_row, nb_col,
               border_mode='same', subsample=(1, 1),
@@ -23,13 +19,14 @@ def conv2d_bn(x, nb_filter, nb_row, nb_col,
     else:
         bn_name = None
         conv_name = None
-    bn_axis = 1
-    x = Convolution2D(nb_filter, nb_row, nb_col,
-                      subsample=subsample,
-                      activation='relu',
-                      border_mode=border_mode,
+    bn_axis = 3
+    x = Conv2D(nb_filter, (nb_row, nb_col),
+                      strides=subsample,
+                      padding=border_mode,
+                      use_bias=False,
                       name=conv_name)(x)
-    x = BatchNormalization(axis=bn_axis, name=bn_name)(x)
+    x = BatchNormalization(axis=bn_axis, scale=False, name=bn_name)(x)
+    x = Activation('relu', name=name)(x)
     return x
 
 def inception_v3_model(img_rows, img_cols, channel=1, num_classes=None):
@@ -47,8 +44,9 @@ def inception_v3_model(img_rows, img_cols, channel=1, num_classes=None):
       channel - 1 for grayscale, 3 for color 
       num_classes - number of class labels for our classification task
     """
-    channel_axis = 1
-    img_input = Input(shape=(channel, img_rows, img_cols))
+
+    channel_axis = 3
+    img_input = Input(shape=(img_rows, img_cols, channel))
     x = conv2d_bn(img_input, 32, 3, 3, subsample=(2, 2), border_mode='valid')
     x = conv2d_bn(x, 32, 3, 3, border_mode='valid')
     x = conv2d_bn(x, 64, 3, 3)
@@ -59,22 +57,54 @@ def inception_v3_model(img_rows, img_cols, channel=1, num_classes=None):
     x = MaxPooling2D((3, 3), strides=(2, 2))(x)
 
     # mixed 0, 1, 2: 35 x 35 x 256
-    for i in range(3):
-        branch1x1 = conv2d_bn(x, 64, 1, 1)
+    
+    branch1x1 = conv2d_bn(x, 64, 1, 1)
 
-        branch5x5 = conv2d_bn(x, 48, 1, 1)
-        branch5x5 = conv2d_bn(branch5x5, 64, 5, 5)
+    branch5x5 = conv2d_bn(x, 48, 1, 1)
+    branch5x5 = conv2d_bn(branch5x5, 64, 5, 5)
 
-        branch3x3dbl = conv2d_bn(x, 64, 1, 1)
-        branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
-        branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+    branch3x3dbl = conv2d_bn(x, 64, 1, 1)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
 
-        branch_pool = AveragePooling2D(
-            (3, 3), strides=(1, 1), border_mode='same')(x)
-        branch_pool = conv2d_bn(branch_pool, 32, 1, 1)
-        x = merge([branch1x1, branch5x5, branch3x3dbl, branch_pool],
-                  mode='concat', concat_axis=channel_axis,
-                  name='mixed' + str(i))
+    branch_pool = AveragePooling2D(
+        (3, 3), strides=(1, 1), padding='same')(x)
+    branch_pool = conv2d_bn(branch_pool, 32, 1, 1)
+    x = concatenate([branch1x1, branch5x5, branch3x3dbl, branch_pool],
+              axis=channel_axis,
+              name='mixed0')
+
+    branch1x1 = conv2d_bn(x, 64, 1, 1)
+
+    branch5x5 = conv2d_bn(x, 48, 1, 1)
+    branch5x5 = conv2d_bn(branch5x5, 64, 5, 5)
+
+    branch3x3dbl = conv2d_bn(x, 64, 1, 1)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+
+    branch_pool = AveragePooling2D(
+        (3, 3), strides=(1, 1), padding='same')(x)
+    branch_pool = conv2d_bn(branch_pool, 64, 1, 1)
+    x = concatenate([branch1x1, branch5x5, branch3x3dbl, branch_pool],
+              axis=channel_axis,
+              name='mixed1')
+
+    branch1x1 = conv2d_bn(x, 64, 1, 1)
+
+    branch5x5 = conv2d_bn(x, 48, 1, 1)
+    branch5x5 = conv2d_bn(branch5x5, 64, 5, 5)
+
+    branch3x3dbl = conv2d_bn(x, 64, 1, 1)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+
+    branch_pool = AveragePooling2D(
+        (3, 3), strides=(1, 1), padding='same')(x)
+    branch_pool = conv2d_bn(branch_pool, 64, 1, 1)
+    x = concatenate([branch1x1, branch5x5, branch3x3dbl, branch_pool],
+              axis=channel_axis,
+              name='mixed2')
 
     # mixed 3: 17 x 17 x 768
     branch3x3 = conv2d_bn(x, 384, 3, 3, subsample=(2, 2), border_mode='valid')
@@ -85,8 +115,8 @@ def inception_v3_model(img_rows, img_cols, channel=1, num_classes=None):
                              subsample=(2, 2), border_mode='valid')
 
     branch_pool = MaxPooling2D((3, 3), strides=(2, 2))(x)
-    x = merge([branch3x3, branch3x3dbl, branch_pool],
-              mode='concat', concat_axis=channel_axis,
+    x = concatenate([branch3x3, branch3x3dbl, branch_pool],
+              axis=channel_axis,
               name='mixed3')
 
     # mixed 4: 17 x 17 x 768
@@ -102,10 +132,10 @@ def inception_v3_model(img_rows, img_cols, channel=1, num_classes=None):
     branch7x7dbl = conv2d_bn(branch7x7dbl, 128, 7, 1)
     branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 1, 7)
 
-    branch_pool = AveragePooling2D((3, 3), strides=(1, 1), border_mode='same')(x)
+    branch_pool = AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
     branch_pool = conv2d_bn(branch_pool, 192, 1, 1)
-    x = merge([branch1x1, branch7x7, branch7x7dbl, branch_pool],
-              mode='concat', concat_axis=channel_axis,
+    x = concatenate([branch1x1, branch7x7, branch7x7dbl, branch_pool],
+              axis=channel_axis,
               name='mixed4')
 
     # mixed 5, 6: 17 x 17 x 768
@@ -123,10 +153,10 @@ def inception_v3_model(img_rows, img_cols, channel=1, num_classes=None):
         branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 1, 7)
 
         branch_pool = AveragePooling2D(
-            (3, 3), strides=(1, 1), border_mode='same')(x)
+            (3, 3), strides=(1, 1), padding='same')(x)
         branch_pool = conv2d_bn(branch_pool, 192, 1, 1)
-        x = merge([branch1x1, branch7x7, branch7x7dbl, branch_pool],
-                  mode='concat', concat_axis=channel_axis,
+        x = concatenate([branch1x1, branch7x7, branch7x7dbl, branch_pool],
+                  axis=channel_axis,
                   name='mixed' + str(5 + i))
 
     # mixed 7: 17 x 17 x 768
@@ -136,16 +166,16 @@ def inception_v3_model(img_rows, img_cols, channel=1, num_classes=None):
     branch7x7 = conv2d_bn(branch7x7, 192, 1, 7)
     branch7x7 = conv2d_bn(branch7x7, 192, 7, 1)
 
-    branch7x7dbl = conv2d_bn(x, 160, 1, 1)
+    branch7x7dbl = conv2d_bn(x, 192, 1, 1)
     branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 7, 1)
     branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 1, 7)
     branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 7, 1)
     branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 1, 7)
 
-    branch_pool = AveragePooling2D((3, 3), strides=(1, 1), border_mode='same')(x)
+    branch_pool = AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
     branch_pool = conv2d_bn(branch_pool, 192, 1, 1)
-    x = merge([branch1x1, branch7x7, branch7x7dbl, branch_pool],
-              mode='concat', concat_axis=channel_axis,
+    x = concatenate([branch1x1, branch7x7, branch7x7dbl, branch_pool],
+              axis=channel_axis,
               name='mixed7')
 
     # mixed 8: 8 x 8 x 1280
@@ -160,8 +190,8 @@ def inception_v3_model(img_rows, img_cols, channel=1, num_classes=None):
                             subsample=(2, 2), border_mode='valid')
 
     branch_pool = AveragePooling2D((3, 3), strides=(2, 2))(x)
-    x = merge([branch3x3, branch7x7x3, branch_pool],
-              mode='concat', concat_axis=channel_axis,
+    x = concatenate([branch3x3, branch7x7x3, branch_pool],
+              axis=channel_axis,
               name='mixed8')
 
     # mixed 9: 8 x 8 x 2048
@@ -171,22 +201,22 @@ def inception_v3_model(img_rows, img_cols, channel=1, num_classes=None):
         branch3x3 = conv2d_bn(x, 384, 1, 1)
         branch3x3_1 = conv2d_bn(branch3x3, 384, 1, 3)
         branch3x3_2 = conv2d_bn(branch3x3, 384, 3, 1)
-        branch3x3 = merge([branch3x3_1, branch3x3_2],
-                          mode='concat', concat_axis=channel_axis,
+        branch3x3 = concatenate([branch3x3_1, branch3x3_2],
+                          axis=channel_axis,
                           name='mixed9_' + str(i))
 
         branch3x3dbl = conv2d_bn(x, 448, 1, 1)
         branch3x3dbl = conv2d_bn(branch3x3dbl, 384, 3, 3)
         branch3x3dbl_1 = conv2d_bn(branch3x3dbl, 384, 1, 3)
         branch3x3dbl_2 = conv2d_bn(branch3x3dbl, 384, 3, 1)
-        branch3x3dbl = merge([branch3x3dbl_1, branch3x3dbl_2],
-                             mode='concat', concat_axis=channel_axis)
+        branch3x3dbl = concatenate([branch3x3dbl_1, branch3x3dbl_2],
+                             axis=channel_axis)
 
         branch_pool = AveragePooling2D(
-            (3, 3), strides=(1, 1), border_mode='same')(x)
+            (3, 3), strides=(1, 1), padding='same')(x)
         branch_pool = conv2d_bn(branch_pool, 192, 1, 1)
-        x = merge([branch1x1, branch3x3, branch3x3dbl, branch_pool],
-                  mode='concat', concat_axis=channel_axis,
+        x = concatenate([branch1x1, branch3x3, branch3x3dbl, branch_pool],
+                  axis=channel_axis,
                   name='mixed' + str(9 + i))
 
     # Fully Connected Softmax Layer
@@ -198,7 +228,7 @@ def inception_v3_model(img_rows, img_cols, channel=1, num_classes=None):
     model = Model(img_input, x_fc)
 
     # Load ImageNet pre-trained data 
-    model.load_weights('imagenet_models/inception_v3_weights_th_dim_ordering_th_kernels.h5')
+    model.load_weights('imagenet_models/inception_v3_weights_tf_dim_ordering_tf_kernels.h5')
 
     # Truncate and replace softmax layer for transfer learning
     # Cannot use model.layers.pop() since model is not of Sequential() type
@@ -214,36 +244,4 @@ def inception_v3_model(img_rows, img_cols, channel=1, num_classes=None):
     sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
 
-    return model 
-
-if __name__ == '__main__':
-
-    # Example to fine-tune on 3000 samples from Cifar10
-
-    img_rows, img_cols = 299, 299 # Resolution of inputs
-    channel = 3
-    num_classes = 10 
-    batch_size = 16 
-    nb_epoch = 10
-
-    # Load Cifar10 data. Please implement your own load_data() module for your own dataset
-    X_train, Y_train, X_valid, Y_valid = load_cifar10_data(img_rows, img_cols)
-
-    # Load our model
-    model = inception_v3_model(img_rows, img_cols, channel, num_classes)
-
-    # Start Fine-tuning
-    model.fit(X_train, Y_train,
-              batch_size=batch_size,
-              nb_epoch=nb_epoch,
-              shuffle=True,
-              verbose=1,
-              validation_data=(X_valid, Y_valid),
-              )
-
-    # Make predictions
-    predictions_valid = model.predict(X_valid, batch_size=batch_size, verbose=1)
-
-    # Cross-entropy loss score
-    score = log_loss(Y_valid, predictions_valid)
-
+    return model
